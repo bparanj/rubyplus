6bf09e2ab03a8009c0f11c28aa1997fe9674cda3
Bala Paranj <bparanj@gmail.com>
2011-05-20 22:12:04 -0700

    first commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Rubyplus::Application.config.secret_token = '0c3ff32aacbb91cfec0f202c7704f64b8c31ccada993d69af3a8cfd5f1a9d9cb3870f87511e687d554fdbe41943178ffb34b20a0667c11c6ded4d0445f6d3299'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,8 @@
+# Be sure to restart your server when you modify this file.
+
+Rubyplus::Application.config.session_store :cookie_store, :key => '_rubyplus_session'
+
+# Use the database for sessions instead of the cookie-based default,
+# which shouldn't be used to store highly confidential information
+# (create the session table with "rails generate session_migration")
+# Rubyplus::Application.config.session_store :active_record_store
